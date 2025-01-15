3c72f3d99f25525b304bc03d5a5d2514032e5954
PabloSeguraLopez <pabseglop@gmail.com>
2025-01-15 12:54:40 +0000

    Corrected go_to_gap function

--- navigation_to_line.py
+++ navigation_to_line.py
@@ -31,7 +31,7 @@ def go_to_line(left_motor, right_motor, color_sensor, angle_to_line):
     # Tras detectar la línea, avanza para que ésta esté en el centro de gravedad del robot
     avanzar_cm(5, left_motor, right_motor, velocidad=20)
 
-def go_to_gap(first_obstacle_distance, second_obstacle_distance, angle_between_obstacles, left_motor, right_motor, color_sensor):
+def go_to_gap(angle_between_obstacles, left_motor, right_motor, color_sensor):
     go_to_line(left_motor, right_motor, color_sensor, angle_between_obstacles/2)
 
 
